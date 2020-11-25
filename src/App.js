6fdaec094eda2f5e43c267bae3462981060c1392
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-11-25 15:02:17 -0800

    folder structure adjusted to deploy on Github Pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom"; // npm install react-router-dom
-import './App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import SearchContainer from "./components/SearchContainer";
